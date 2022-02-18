b9e9c852c8902c62f94fe8c5ef012470f53ddef0
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-02-18 12:29:06 +0200

    Update pixabayAPI.js

--- src/js/pixabayAPI.js
+++ src/js/pixabayAPI.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
+const API_KEY = '25715337-58cde3c0d1b1902de73779f35';
+const BASE_URL = 'https://pixabay.com/api';
 
 export default class NewPixabayAPI {
   constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
-  async axiosPicture() {
-    // console.log(this);
-    const API_KEY = '25715337-58cde3c0d1b1902de73779f35';
-    const BASE_URL = 'https://pixabay.com/api';
 
+  async axiosPicture() {
     try {
       const response = await axios.get(
         `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`,
